ff1ab88ee90bff0bdc2b7b167a50b43b1fc75aa4
刘贤金 <875832935@qq.com>
2024-02-10 18:01:15 +0800

    项目初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,90 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import {Link, Route, Routes, Navigate} from "react-router-dom";
+
+const Wrapper = styled.div`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+`
+const Main = styled.div`
+    flex-grow: 1;
+    overflow: auto;
+`
+const Nav = styled.nav`
+    border: 1px green solid;
+
+    > ul {
+        display: flex;
+        >li{
+            width: 33.3%;
+            text-align: center;
+            padding: 16px;
+        }
+    }
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Main>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home"/>}></Route>
+          <Route
+            path="/tags"
+            element={<Tags/>}
+          />
+          <Route
+            path="/money"
+            element={<Money/>}
+          />
+          <Route
+            path="/statistics"
+            element={<Statistics/>}
+          />
+          <Route path="*" element={<NoMatch/>}/>
+        </Routes>
+      </Main>
+      <Nav>
+        <ul>
+          <li>
+            <Link to="/tags">标签页</Link>
+          </li>
+          <li>
+            <Link to="/money">账本页</Link>
+          </li>
+          <li>
+            <Link to="/statistics">统计页</Link>
+          </li>
+        </ul>
+      </Nav>
+    </Wrapper>
   );
 }
+
+function NoMatch() {
+  return (
+    <h1>404</h1>
+  )
+}
+
+function Tags() {
+  return (
+    <h1>标签页</h1>
+  )
+}
+
+function Money() {
+  return (
+    <h1>账本页</h1>
+  )
+}
+
+function Statistics() {
+  return (
+    <h1>统计页</h1>
+  )
+}
+
 
 export default App;
