dbe9889ea689a9cd7a174cadb4bb73a45f3f0cfc
刘贤金 <875832935@qq.com>
2024-02-18 15:34:32 +0800

    分组完成

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -1,8 +1,7 @@
 import Layout from "../components/Layout";
-import React, {useEffect} from "react";
+import React from "react";
 import CategorySection from "./Money/CategorySection";
 import styled from "styled-components";
-import Space from "../components/Space";
 import useTags from "../hooks/useTags";
 import useRecords, {RecordItem} from "../hooks/useRecords";
 import day from "dayjs"
@@ -22,9 +21,20 @@ const StatisticsWrapper = styled.div`
     justify-content: space-between;
     padding: 10px;
     background-color: #fff;
-    div:nth-child(2) {
+    span:nth-child(2) {
       margin-left: 15px;
       flex: 1;
+    }
+  }
+  .header {
+    justify-content: space-between;
+    span:nth-child(2) {
+      margin-left: 15px;
+      flex: 1;
+      text-align: right;
+    }
+    .total{
+      font-weight: bold;
     }
   }
   div:first-child {
@@ -44,6 +54,13 @@ const Statistics = () => {
     }
     hash[key].push(r);
   })
+  const array = Object.entries(hash).sort((a, b) => {
+    if (a[0] === b[0]) return 0;
+    if (a[0] > b[0]) return 1;
+    if (a[0] < b[0]) return -1;
+    return 0;
+  })
+  console.log(array)
   return (
     <Layout>
       <CategoryWrapper>
@@ -51,18 +68,28 @@ const Statistics = () => {
       </CategoryWrapper>
       <StatisticsWrapper>
         {
-          selectedRecords.map((record) => {
+          array.map(([data, record]) => {
             return (
-              <section key={record.createdAt}>
-                <div>
-                  <span>{day(record.createdAt).format('YYYY年MM月DD日')}</span>
-                  <span>￥124</span>
+              <section key={data}>
+                <div className="header">
+                  <span>{day(data).format('YYYY年MM月DD日')}</span>
+                  <span className="total">￥
+                    <span>{
+                      record.reduce((sum, item) => {
+                        return sum + Number(item.amount);
+                      }, 0).toFixed(2)
+                    }</span>
+                  </span>
                 </div>
-                <div>
-                  <span>{getName(record.tags[0])}</span>
-                  <span>{record.note}</span>
-                  <span>￥{record.amount}</span>
-                </div>
+                {
+                  record.map(record =>
+                    <div key={record.createdAt}>
+                      <span>{getName(record.tags[0])}</span>
+                      <span>{record.note}</span>
+                      <span>￥{Number(record.amount).toFixed(2)}</span>
+                    </div>
+                  )
+                }
               </section>
             )
           })
